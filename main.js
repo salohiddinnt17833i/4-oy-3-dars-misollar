0c2715a3ac08b7b5e0d364c6d2bdd98db2b50dc8
salohiddinnt17833i <salohiddindevoleper@gmail.com>
2023-12-11 16:28:04 +0500

    Mislollar bajarildi

---
+++ main.js
@@ -0,0 +1,99 @@
+//      1   
+// let num1 = 10;
+// let num2 = 11;
+// let num3 = 50;
+// let sum = 1;
+// if (num1 % 2 == 0 && num1 % 5 == 0) {
+//     sum = sum * num1;
+// }
+// if (num2 % 2 == 0 && num2 % 5 == 0) {
+//     sum = sum * num2;
+// }
+// if (num3 % 2 == 0 && num3 % 5 == 0) {
+//     sum = sum * num3;
+// }
+// console.log(sum);
+
+//      2
+// let num1 = 10;
+// let num2 = 1;
+// let num3 = 70;
+// let num4 = 50;
+// let sum = 1;
+// if (num1 % 3 == 0 || num1 % 7 == 0) {
+//     sum = sum * num1;
+// }
+// if (num2 % 3 == 0 || num2 % 7 == 0) {
+//     sum = sum * num2;
+// }
+// if (num3 % 3 == 0 || num3 % 7 == 0) {
+//     sum = sum * num3;
+// }
+// if (num4 % 3 == 0 || num4 % 7 == 0) {
+//     sum = sum * num4;
+// }
+// console.log(sum);
+
+
+//      3
+// let num = 11;
+// let o;
+// let b;
+// let res;
+// o = num % 10;
+// b = num / 10;
+// res = o * b;
+// console.log(Math.round(res));
+
+
+//      4
+// let num = 60;
+// let o;
+// let res;
+// o = num / 10;
+// if (o % 2 == 0) {
+//    res = o * 2;
+// } else {
+//    res = o ** 3;
+// }
+// console.log(res);
+
+
+//      5
+// let num = 123;
+// let y;
+// let o;
+// y = num / 100;
+// o = num / 10 % 10;
+// console.log(Math.round(y));
+// console.log(Math.round(o));
+
+
+//      6
+
+
+//      7
+
+
+//      8
+// let a = 2;
+// let b = 5;
+// let y;
+// let p;
+// p = (a + b) * 2;
+// y = a * b;
+// if (y > p) {
+//     console.log(y);
+// }else {
+//     console.log(p);
+// }
+
+
+//      9
+
+
+
+
+
+
+
